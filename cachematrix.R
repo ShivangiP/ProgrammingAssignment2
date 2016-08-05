2b9c752631d2c222d92372b0d9f4ceb537035bde
ShivangiP <pandey.shivangi85@gmail.com>
2016-08-04 20:57:58 -0700

    adding cachematrix.R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## R Programming Week 3 Assignment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ 
+       m <- NULL
+       
+ set <- function(y) {
+  
+              x <<- y
+            
+    m <<- NULL
+        }
+      
+  get <- function() x
+       
+ setinv <- function(inv)
+ m <<- inv
+        
+getinv <- function() m
+       
+list(set = set, get = get,setinv = setinv,getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## Function will return Matrix inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getinv()
+       
+ if(!is.null(m)) {
+              
+  message("getting cached data")
+       
+         return(m)
+       
+ }
+       
+ data <- x$get()
+    
+    m <- solve(data, ...)
+     
+   x$setinv(m)
+        m
+
 }
